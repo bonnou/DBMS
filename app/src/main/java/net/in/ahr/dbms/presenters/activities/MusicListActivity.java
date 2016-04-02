b77b1418833bd21dbca94c9ec6cc8ab66a73a0b9
bonnou <earthly.desire@gmail.com>
2016-04-02 15:33:36 +0900

    Redmine-GitHub連携テスト15

--- app/src/main/java/net/in/ahr/dbms/presenters/activities/MusicListActivity.java
+++ app/src/main/java/net/in/ahr/dbms/presenters/activities/MusicListActivity.java
@@ -92,6 +92,7 @@ public class MusicListActivity extends AppCompatActivity
         transaction.commit();
 */
 
+
 //            ((CustomApplication) this.getApplicationContext()).getDaoSession().getMusicMstDao().deleteAll();
 
 
