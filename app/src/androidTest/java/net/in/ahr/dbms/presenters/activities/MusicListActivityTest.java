1a50bae4d04142b5d86294f121e8dc6a28e9c2f8
bonnou <earthly.desire@gmail.com>
2016-05-15 20:39:46 +0900

    #23 リファクタリング＆試験自動化検討（Espresso断念した時の残骸、邪魔しないのでコミット）

---
+++ app/src/androidTest/java/net/in/ahr/dbms/presenters/activities/MusicListActivityTest.java
@@ -0,0 +1,62 @@
+package net.in.ahr.dbms.presenters.activities;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import net.in.ahr.dbms.R;
+import net.in.ahr.dbms.androidTest.utils.DisableAnimationsRule;
+import net.in.ahr.dbms.androidTest.utils.TestUtils;
+import net.in.ahr.dbms.data.strage.util.LogUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+/**
+ * Created by str2653z on 16/05/15.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MusicListActivityTest {
+    private static final String TAG = MusicListActivityTest.class.getSimpleName();
+    private Activity mActivity;
+
+    @ClassRule
+    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
+
+    @Rule
+    public ActivityTestRule<MusicListActivity> mActivityRule = new ActivityTestRule(MusicListActivity.class);
+
+    @Before
+    public void setUp(){
+        LogUtil.logDebug("setUp");
+        // Activityの取得
+        mActivity = mActivityRule.getActivity();
+        TestUtils.toggleAnimationEnable(mActivity, TAG, false);
+
+    }
+    // @Testが使えるので日本語だけのテストメソッドが書けるようになった
+    @Test
+    public void espressoTestSample() {
+
+//        onView(withId(R.id.musicListView)).
+
+//        Assert.assertNotNull(mActivity);
+//        onView(withId(R.id.fragment)).check(matches(isDisplayed()));
+//        onView(withId(R.id.musicListView)).check(matches(isDisplayed()));
+
+    }
+
+
+}
